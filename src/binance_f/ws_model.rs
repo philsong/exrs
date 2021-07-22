ada1dabb0d846ed3b5698f228f7a7ca56fe5dbce
zp <zhenpingfeng@gmail.com>
2021-07-22 19:42:45 +0800

    update.

--- src/binance_f/ws_model.rs
+++ src/binance_f/ws_model.rs
@@ -690,7 +690,91 @@ pub struct Position {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
-pub struct OrderTradeEvent {}
+pub struct OrderTradeEvent {
+    #[serde(rename = "e")]
+    pub event_type: String,
+
+    #[serde(rename = "E")]
+    pub event_time: u64,
+
+    #[serde(rename = "s")]
+    pub symbol: String,
+
+    #[serde(rename = "c")]
+    pub new_client_order_id: String,
+
+    #[serde(rename = "S")]
+    pub side: String,
+
+    #[serde(rename = "o")]
+    pub order_type: String,
+
+    #[serde(rename = "f")]
+    pub time_in_force: String,
+
+    #[serde(rename = "q")]
+    pub qty: String,
+
+    #[serde(rename = "p")]
+    pub price: String,
+
+    #[serde(skip, rename = "P")]
+    pub p_ignore: String,
+
+    #[serde(skip, rename = "F")]
+    pub f_ignore: String,
+
+    #[serde(skip)]
+    pub g: i32,
+
+    #[serde(skip, rename = "C")]
+    pub c_ignore: Option<String>,
+
+    #[serde(rename = "x")]
+    pub execution_type: String,
+
+    #[serde(rename = "X")]
+    pub order_status: String,
+
+    #[serde(rename = "r")]
+    pub order_reject_reason: String,
+
+    #[serde(rename = "i")]
+    pub order_id: u64,
+
+    #[serde(rename = "l")]
+    pub qty_last_filled_trade: String,
+
+    #[serde(rename = "z")]
+    pub accumulated_qty_filled_trades: String,
+
+    #[serde(rename = "L")]
+    pub price_last_filled_trade: String,
+
+    #[serde(rename = "n")]
+    pub commission: String,
+
+    #[serde(skip, rename = "N")]
+    pub asset_commisioned: Option<String>,
+
+    #[serde(rename = "T")]
+    pub trade_order_time: u64,
+
+    #[serde(rename = "t")]
+    pub trade_id: i64,
+
+    #[serde(skip, rename = "I")]
+    pub i_ignore: u64,
+
+    #[serde(skip)]
+    pub w: bool,
+
+    #[serde(rename = "m")]
+    pub is_buyer_maker: bool,
+
+    #[serde(skip, rename = "M")]
+    pub m_ignore: bool,
+}
 
 pub(crate) mod string_or_float {
     use std::fmt;
