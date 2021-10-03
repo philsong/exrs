bd3aa8a3a578295e54f514d9fdfc54ec4d064ab6
zhenpingfeng <zhenpingfeng@gmail.com>
2021-10-03 23:06:39 +0800

    Update OKEX v5 api.

---
+++ src/okex_v5/rest_model.rs
@@ -0,0 +1,122 @@
+/// Order type
+/// market: market order
+/// limit: limit order
+/// post_only: Post-only order
+/// fok: Fill-or-kill order
+/// ioc: Immediate-or-cancel order
+/// optimal_limit_ioc :Market order with immediate-or-cancel order
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum OrderType {
+    Limit,
+    Market,
+    PostOnly,
+    FOK,
+    IOC,
+    OptimalLimitIoc,
+    #[serde(other)]
+    Other,
+}
+
+pub mod string_or_float {
+    use std::fmt;
+
+    use serde::{de, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: fmt::Display,
+        S: Serializer,
+    {
+        serializer.collect_str(value)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<f64, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum StringOrFloat {
+            String(String),
+            Float(f64),
+        }
+
+        match StringOrFloat::deserialize(deserializer)? {
+            StringOrFloat::String(s) => s.parse().map_err(de::Error::custom),
+            StringOrFloat::Float(i) => Ok(i),
+        }
+    }
+}
+
+pub(crate) mod string_or_float_opt {
+    use std::fmt;
+
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<T, S>(value: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: fmt::Display,
+        S: Serializer,
+    {
+        match value {
+            Some(v) => super::string_or_float::serialize(v, serializer),
+            None => serializer.serialize_none(),
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<f64>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum StringOrFloat {
+            String(String),
+            Float(f64),
+        }
+
+        Ok(Some(super::string_or_float::deserialize(deserializer)?))
+    }
+}
+
+pub(crate) mod string_or_bool {
+    use std::fmt;
+
+    use serde::{de, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: fmt::Display,
+        S: Serializer,
+    {
+        serializer.collect_str(value)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<bool, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum StringOrFloat {
+            String(String),
+            Bool(bool),
+        }
+
+        match StringOrFloat::deserialize(deserializer)? {
+            StringOrFloat::String(s) => s.parse().map_err(de::Error::custom),
+            StringOrFloat::Bool(i) => Ok(i),
+        }
+    }
+}
+
+fn default_stop_price() -> f64 {
+    0.0
+}
+fn default_activation_price() -> f64 {
+    0.0
+}
+fn default_price_rate() -> f64 {
+    0.0
+}
--- src/okex_v5/websockets.rs
+++ src/okex_v5/websockets.rs
@@ -1,7 +1,9 @@
 use super::config;
 use super::config::*;
 use super::errors::*;
+use super::rest_model::{OrderType, string_or_bool, string_or_float, string_or_float_opt};
 
+use awc::ws::Message;
 use log::debug;
 use std::sync::atomic::{AtomicBool, Ordering};
 
@@ -11,8 +13,10 @@ use awc::{
     BoxedSocket, Client, ClientResponse,
 };
 use futures_util::{sink::SinkExt as _, stream::StreamExt as _};
+use serde::{Serialize, Deserialize};
 use serde_json::from_slice;
 use tokio::sync::mpsc;
+use uuid::Uuid;
 
 pub enum WebsocketAPI {
     Public,
@@ -121,11 +125,93 @@ impl<WE: serde::de::DeserializeOwned> WebSockets<WE> {
     }
 
     // trade start from here
-    pub async fn place_order() {
+    async fn place_order(&mut self, order: WSOrder) -> Result<()> {
+        if let Some((_, ref mut socket)) = self.socket {
+
+            let ws_order = WSOrderRequest {
+                id: Uuid::new_v4().to_string(),
+                op: "order".to_string(),
+                args: vec![order],
+            };
+
+            let text = serde_json::to_string(&ws_order)?;
+            socket.send(Message::Text(text.into())).await?;
+            Ok(())
+        } else {
+            Err(Error::Msg("Not able to send requests".to_string()))
+        }
+    }
+    
+    async fn place_multipy_order(&mut self, orders: Vec<WSOrder>) -> Result<()> {
+        if let Some((_, ref mut socket)) = self.socket {
+
+            let ws_orders = WSOrderRequest {
+                id: Uuid::new_v4().to_string(),
+                op: "batch-orders".to_string(),
+                args: orders,
+            };
+
+            let text = serde_json::to_string(&ws_orders)?;
+            socket.send(Message::Text(text.into())).await?;
+            Ok(())
+        } else {
+            Err(Error::Msg("Not able to send requests".to_string()))
+        }
+    }
+
+    pub async fn limit_buy(&mut self, 
+        symbol: impl Into<String>,
+        qty: impl Into<String>,
+        price: impl Into<String>,
+        order_type: OrderType,
+    ) -> Result<()> {
+        let order = WSOrder {
+            symbol: symbol.into(),
+            trade_mode: TradeMode::Cross,
+            currency: None,
+            client_order_id: None,
+            tag: None,
+            side: OrderSide::Buy,
+            position_side: None, // None for net mode
+            order_type: order_type,
+            qty: qty.into(),
+            price: Some(price.into()),
+            reduce_only: None,
+            target_currency: None,
+        };
+        self.place_order(order).await?;
+        Ok(())
+    }
+
+    pub async fn limit_sell(&mut self, 
+        symbol: impl Into<String>,
+        qty: impl Into<String>,
+        price: impl Into<String>,
+        order_type: OrderType
+    ) -> Result<()> {
+        let order = WSOrder {
+            symbol: symbol.into(),
+            trade_mode: TradeMode::Cross,
+            currency: None,
+            client_order_id: None,
+            tag: None,
+            side: OrderSide::Sell,
+            position_side: None, // None for net mode
+            order_type: order_type,
+            qty: qty.into(),
+            price: Some(price.into()),
+            reduce_only: None,
+            target_currency: None,
+        };
+        self.place_order(order).await?;
+        Ok(())
+    }
+
+    pub async fn market_buy() {
 
     }
 
-    pub async fn place_multiple_orders() {
+    pub async fn market_sell() {
 
     }
 
@@ -140,4 +226,74 @@ impl<WE: serde::de::DeserializeOwned> WebSockets<WE> {
     pub async fn amend_multiple_order() {
 
     }
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum PositionSide {
+    Net,
+    Long,
+    Short,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum OrderSide {
+    Buy,
+    Sell,
+}
+
+/// By default, buy
+impl Default for OrderSide {
+    fn default() -> Self {
+        Self::Buy
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum TradeMode {
+    Isolated,
+    Cross,
+    Cash,
+}
+
+/// By default, Cross
+impl Default for TradeMode {
+    fn default() -> Self {
+        Self::Cross
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WSOrderRequest {
+    pub id: String,
+    pub op: String,
+    pub args: Vec<WSOrder>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WSOrder {
+    #[serde(rename = "inst_id")]
+    pub symbol: String,
+    #[serde(rename = "td_mode")]
+    pub trade_mode: TradeMode,
+    #[serde(rename = "ccy")]
+    pub currency: Option<String>,
+    #[serde(rename = "clOrdId")]
+    pub client_order_id: Option<String>,
+    pub tag: Option<String>,
+    pub side: OrderSide,
+    #[serde(rename = "posSide")]
+    pub position_side: Option<String>,
+    #[serde(rename = "ord_type")]
+    pub order_type: OrderType,
+    #[serde(rename = "sz")]
+    pub qty: String,
+    #[serde(rename = "px")]
+    pub price: Option<String>,
+    pub reduce_only: Option<bool>,
+    #[serde(rename = "tgtCcy")]
+    pub target_currency: Option<String>,
 }
